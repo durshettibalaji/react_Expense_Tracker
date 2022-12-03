5413cc1176f9331205483d768c70065e5428a633
Balaji Durshetti <durshettibalaji1@gmail.com>
2022-12-04 01:09:11 +0530

    assigment done

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Expenseitem.css'
+import './ExpenseDate.js'
+import ExpenseDate from './ExpenseDate.js'
+
+export default function ExpenseItem(props) {
+
+  return (
+    <>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>{props.amount}</div>
+      </div>
+    </div>
+
+    </>
+  )
+}
